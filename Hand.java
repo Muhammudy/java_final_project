b4f8cdcc5e3afd40b250edc4bf99be32d8776171
Sebastian Froning <sebfroning@Sebastians-MacBook-Pro.local>
2025-04-20 16:36:26 -0500

    added new functions

--- Hand.java
+++ Hand.java
@@ -10,4 +10,52 @@ public class Hand {
 
     }
 
+    public void addCard(Card card) { //added for testing, may or may not keep
+        this.hand.add(card);
+    }
+
+    public int getValue(){// returns value of hand and accounts for aces
+        int total = 0;
+        int aces = 0;
+        for(Card card : hand){
+            if(card.getValue() == 11){
+                aces++;
+                total += card.getValue();
+            }
+            else{
+                total += card.getValue();
+            }
+        }
+
+        for(int i = 0; i < aces; ++i){
+            if(total > 21){
+                total -= 10;
+
+            }
+            else {break;}
+        }
+        return total;
+    }
+
+    public boolean isBlackjack(){
+        return this.getValue() == 21;
+    }
+
+    public boolean isBusted(){
+        return this.getValue() > 21;
+    }
+
+    public boolean canSplit(){
+        if(hand.size() != 2){
+            return false;
+        }
+
+        else if(hand.get(0).getValue() == hand.get(1).getValue()){
+            return true;
+        }
+
+        else{
+            return false;
+        }
+    }
 }
